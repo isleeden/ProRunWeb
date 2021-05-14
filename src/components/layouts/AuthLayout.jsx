63adf95e9d1ed9afdc34e6a094fabeec6c3f861a
isleeden <nkoreanarmy@gmail.com>
2021-05-14 19:20:09 +0500

    implement layout footer

--- src/components/layouts/AuthLayout.jsx
+++ src/components/layouts/AuthLayout.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
-import styles from './AuthLayout.module.scss'
-import logo from '../../assets/images/icons/logo.svg'
+import React from "react";
+import styles from "./AuthLayout.module.scss";
+import logo from "../../assets/images/icons/logo.svg";
 
-const AuthLayout = ({children, title, subtitle}) => {
+const AuthLayout = ({ children, title, subtitle }) => {
   return (
     <div className={styles.page}>
       <div className={`container ${styles.container}`}>
-        <a href="/">
-          <img className={styles.logo} src={logo} alt="logo" />
-        </a>
-        <div className={styles.form}>
-          <p className={styles.formTitle}>{title}</p>
-          <p className={styles.formSubTitle}>{subtitle}</p>
-          {children}
+        <div className={styles.main}>
+          <a href="/">
+            <img className={styles.logo} src={logo} alt="logo" />
+          </a>
+          <div className={styles.form}>
+            <p className={styles.formTitle}>{title}</p>
+            <p className={styles.formSubTitle}>{subtitle}</p>
+            {children}
+          </div>
+        </div>
+        <div className={styles.footer}>
+          <div className={styles.footerLeft}>
+            <p className={styles.copyright}>© 2017–2020 ООО «ProRun»</p>
+            <a href="/" className={styles.footerLink}>
+              Пользовательское соглашение
+            </a>
+          </div>
+          <div>
+            <a href="/" className={styles.footerLink}>
+              Русский
+            </a>
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default AuthLayout;
