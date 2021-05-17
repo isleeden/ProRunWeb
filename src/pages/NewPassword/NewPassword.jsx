0bc98e0ca0f6d4713f59f9b08d9e98b8a728fb26
isleeden <nkoreanarmy@gmail.com>
2021-05-17 12:09:01 +0500

    implement new password page

---
+++ src/pages/NewPassword/NewPassword.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Button from "../../components/Button/Button";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import PasswordInput from "../../components/PasswordInput/PasswordInput"
+import passwordPage from "../../assets/images/icons/passwordPage.svg"
+
+const NewPassword = () => {
+  return (
+    <AuthLayout
+      title="Придумайте новый пароль"
+      subtitle="Защитите свой аккаунт от взломов"
+      icon={passwordPage}
+      steps="Шаг 1 из 4"
+    >
+      <PasswordInput errorText="Неверный пароль" />
+      <Button buttonText="Далее" valid={false} />
+    </AuthLayout>
+  );
+};
+
+export default NewPassword;
