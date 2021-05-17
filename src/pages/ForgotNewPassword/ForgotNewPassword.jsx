eefa7ac354b9100598d0d3bd2d9b724e8fb1a14c
isleeden <nkoreanarmy@gmail.com>
2021-05-17 12:09:24 +0500

    implement forgot - new password page

---
+++ src/pages/ForgotNewPassword/ForgotNewPassword.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Button from "../../components/Button/Button";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import PasswordInput from "../../components/PasswordInput/PasswordInput"
+import passwordPage from "../../assets/images/icons/passwordPage.svg"
+
+const ForgotPassword = () => {
+  return (
+    <AuthLayout
+      title="Придумайте пароль"
+      subtitle="Защитите свой аккаунт от взломов"
+      icon={passwordPage}
+    >
+      <PasswordInput errorText="Неверный пароль" />
+      <Button buttonText="Сохранить" valid={false} />
+    </AuthLayout>
+  );
+};
+
+export default ForgotPassword;
