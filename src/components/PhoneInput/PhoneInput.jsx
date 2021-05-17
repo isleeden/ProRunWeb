5b86d4e6293a66fd56490b0d7f55dfae116c2685
isleeden <nkoreanarmy@gmail.com>
2021-05-17 09:22:28 +0500

    input completed

--- src/components/PhoneInput/PhoneInput.jsx
+++ src/components/PhoneInput/PhoneInput.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./PhoneInput.module.scss";
 import selectArrow from "../../assets/images/icons/selectArrow.svg";
-import InputMask from 'react-input-mask'
+import InputMask from "react-input-mask";
 
 const options = [
   { country: "Узбекистан", number: "998" },
@@ -21,16 +21,49 @@ const options = [
 const PhoneInput = () => {
   const [currentCountry, setCurrentCountry] = useState(options[0].number);
   const [isSelectOpen, setIsSelectOpen] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+
+  const focusHandler = () => {
+    setIsFocused(true);
+  };
+
+  const blurHandler = () => {
+    setIsFocused(false);
+  };
+
+  const optionsElement = useRef();
+  const selectElement = useRef();
+
+  const outsideSelectClickHandler = (e) => {
+    if (
+      !optionsElement.current.contains(e.target) &&
+      !selectElement.current.contains(e.target)
+    ) {
+      setIsSelectOpen(false);
+    }
+  };
 
   const optionClickHandler = (number) => {
     setCurrentCountry(number);
-    setIsSelectOpen(!isSelectOpen);
+    setIsSelectOpen(false);
   };
 
+  useEffect(() => {
+    document.addEventListener("mousedown", outsideSelectClickHandler);
+    return () => {
+      document.removeEventListener("mousedown", outsideSelectClickHandler);
+    };
+  }, []);
+
   return (
-    <div className={styles.phoneForm}>
+    <div
+      className={`${styles.phoneForm} ${
+        isFocused ? styles.phoneFormFocused : ""
+      }`}
+    >
       <div
         className={styles.select}
+        ref={selectElement}
         onClick={() => setIsSelectOpen(!isSelectOpen)}
       >
         <div className={styles.currentCountry}>+{currentCountry}</div>
@@ -43,11 +76,19 @@ const PhoneInput = () => {
         />
       </div>
       <div className={styles.separator}></div>
-      <InputMask className={styles.input} type="text" mask="99 999 99 99" placeholder="71 123 45 67" />
+      <InputMask
+        className={styles.input}
+        type="text"
+        mask="99 999 99 99"
+        placeholder="71 123 45 67"
+        onFocus={focusHandler}
+        onBlur={blurHandler}
+      />
       <div
         className={`${styles.options} ${
           isSelectOpen ? styles.optionsActive : ""
         }`}
+        ref={optionsElement}
       >
         {options.map(({ country, number }, index) => {
           return (
