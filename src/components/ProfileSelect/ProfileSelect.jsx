04e91d8cfd3e16062a9c55a5646991169d0b4b1b
isleeden <nkoreanarmy@gmail.com>
2021-05-25 14:54:49 +0500

    implement profile select

---
+++ src/components/ProfileSelect/ProfileSelect.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Select from "../Select/Select";
+import styles from "./ProfileSelect.module.scss";
+
+const ProfileSelect = ({ options, label, value, setValue }) => {
+  return (
+    <div className={styles.profileSelect}>
+      <label>{label}</label>
+      <Select
+        className={styles.options}
+        options={options}
+        value={value}
+        setValue={setValue}
+      />
+    </div>
+  );
+};
+
+export default ProfileSelect;
