993c5c54e9ac495d71169d9a4e098c00751d0e52
isleeden <nkoreanarmy@gmail.com>
2021-05-25 15:42:42 +0500

    added profile imput disable state

--- src/components/ProfileInput/ProfileInput.jsx
+++ src/components/ProfileInput/ProfileInput.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import styles from "./ProfileInput.module.scss";
 
-const ProfileInput = ({ label, id, value, onChange }) => {
+const ProfileInput = ({ label, id, value, onChange, disable }) => {
   return (
-    <div className={styles.profileInput}>
+    <div className={`${styles.profileInput} ${disable ? styles.disable : ""}`}>
       <label htmlFor={id}>{label}</label>
-      <input type="text" id={id} value={value} onChange={onChange} />
+      <input
+        type="text"
+        id={id}
+        value={value}
+        onChange={onChange}
+        disabled={disable ? "disabled" : ""}
+      />
     </div>
   );
 };
